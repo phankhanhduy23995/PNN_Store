f66f0f3ee32198cab3edda0edf00b3b59ba12607
Duy Phan <duypk@imt-soft.com>
2019-07-18 15:12:46 +0700

    - refactor app.js

--- app.js
+++ app.js
@@ -1,10 +1,81 @@
-require('./helpers/db')
 const express = require('express');
-const app = express();
-const parser = require('body-parser');
-app.use(parser.json())
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
 
-const userRouter = require('./controllers/user.router');
-app.use('/user', userRouter);
+// [SH] Bring in the data model
+require('./lib/db');
+// [SH] Bring in the Passport config after model is defined
+require('./config/passport');
+// [SH] Bring in the routes for the API (delete the default routes)
+const routesApi = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// [SH] Initialise Passport before using the route middleware
+app.use(passport.initialize());
+
+// [SH] Use the API routes when path starts with /api
+app.use('/', routesApi);
+
+/**
+ * Allow any method from any host and log requests
+ */
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  } else {
+    console.log(`${req.ip} ${req.method} ${req.url}`);
+    next();
+  }
+});
+
+// error handlers
+
+/**
+ * [SH] Catch unauthorised errors
+ */
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({ "message": err.name + ": " + err.message });
+  }
+});
+
+// development error handler
+/**
+ * Catch 404 and forward to error handler
+ */
+app.use(function (req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/**
+ * no stacktraces leaked to user
+ */
+app.use(function (err, req, res) {
+  res.status(err.status || 500)
+    .json({
+      success: false,
+      message: err.message,
+      code: err.status
+    });
+});
 
 module.exports = app;
